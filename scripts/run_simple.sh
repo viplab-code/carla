46fdec27ed4cb4b04b6d972f483674969ba4e38b
Visual Intelligence and Platform (VIP) Lab. <72518471+viplab-code@users.noreply.github.com>
2025-07-24 03:03:46 +0900

    Create run_simple.sh

---
+++ scripts/run_simple.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#for i in {1..10}
+for i in {1..1}
+do
+    echo "=============================="
+    echo "사이클 $i 시작"
+    ./CarlaUE4.sh -RenderOffScreen -nosound -quality-level=Low &
+    sleep 5
+
+    echo "사이클 $i: 데이터 캡처 시작"
+    python ./PythonAPI/samples/run_capture_data.py
+    echo "사이클 $i: 데이터 캡처 완료"
+
+    # kill Carla
+	(sleep 3); kill -9 `ps | grep 'CarlaUE4-Linux-' | awk '{print $1}'`; (sleep 1)
+    echo "사이클 $i 끝!"
+    echo "=============================="
+done
